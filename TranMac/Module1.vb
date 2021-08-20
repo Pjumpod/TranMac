20001be3153c7532024967955910d2d1139978e9
Jumpod Plekhongthu <pjumpod@gmail.com>
2021-08-20 15:19:14 +0700

    Initial version.
    Initial version.

---
+++ TranMac/Module1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Console
+Imports System.String
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+
+Module Module1
+    Private m_DataReceived As New StringBuilder
+    Sub Main()
+        m_DataReceived.Clear()
+        Dim clArgs() As String = Environment.GetCommandLineArgs()
+
+        Dim args As String = String.Empty
+
+        For i As Integer = 1 To clArgs.Count() - 1
+            args += " " + clArgs(i)
+        Next
+        args = args.Replace("'", Convert.ToChar(34)).Replace("|", Convert.ToChar(34)).Replace("@", Convert.ToChar(34))
+
+        Dim PrcStartInfo As New ProcessStartInfo With {.CreateNoWindow = True,
+                                                   .RedirectStandardError = True,
+                                                   .RedirectStandardOutput = True,
+                                                   .UseShellExecute = False,
+                                                   .Arguments = String.Format(args),
+                                                   .FileName = "SerialConsole.exe"}
+
+        Dim Prc As New Process()
+        With Prc.StartInfo
+            .FileName = "SerialConsole.exe"
+            .CreateNoWindow = True
+            .UseShellExecute = False
+            .RedirectStandardOutput = True
+            '.RedirectStandardError = True
+            .Arguments = args
+        End With
+        Prc.Start()
+        Dim std_out As String = Prc.StandardOutput.ReadToEnd()
+        Prc.WaitForExit()
+        'Dim std_out As String = m_DataReceived.ToString()
+
+        'Dim clArgs() As String = Environment.GetCommandLineArgs()
+        'Dim args As String = String.Empty
+
+        'For i As Integer = 1 To clArgs.Count() - 1
+        'args += clArgs(i)
+        'Next
+        std_out = Regex.Replace(std_out, "^\s+$[\r\n]*", "", RegexOptions.Multiline)
+        If std_out.Length() > 1 Then
+
+            std_out = std_out.Trim().Replace(">", "").Replace("device:", "").Replace("0x", "").Replace(" ", "")
+            std_out = Regex.Replace(std_out, "^\s+$[\r\n]*", "", RegexOptions.Multiline)
+        End If
+        'System.Console.Clear()
+        'System.Console.WriteLine(args)
+        System.Console.Write(std_out.Trim())
+    End Sub
+
+    Private Sub HandleDataReceived(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+
+        m_DataReceived.Append(e.Data)
+
+    End Sub
+
+End Module
---
+++ TranMac/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TranMac")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("TranMac")> 
+<Assembly: AssemblyCopyright("Copyright ©  2021")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("23070b6b-7e2f-49e3-88e9-4549e6c5d8ff")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
